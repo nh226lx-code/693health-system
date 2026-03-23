70170a8fe39b371be4c38d24363bca6264472a2a
nh226lx-code <nh226lx@gmail.com>
2026-03-22 20:24:24 -0700

    debug routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import HealthForm from "./pages/HealthForm";
-import Chart from "./pages/Chart";
-import Advice from "./pages/Advice";
-import History from "./pages/History";
-import Stats from "./pages/Stats";
-
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/data-entry" element={<HealthForm />} />
-      <Route path="/analysis" element={<Chart />} />
-      <Route path="/advice" element={<Advice />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/stats" element={<Stats />} />
+      <Route path="/" element={<h1>首页 OK</h1>} />
+      <Route path="/register" element={<h1>注册页 OK</h1>} />
+      <Route path="/login" element={<h1>登录页 OK</h1>} />
     </Routes>
   );
 }
