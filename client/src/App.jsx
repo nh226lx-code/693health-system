35c484d7259f05c2e831ec82237156bf5a529227
nh226lx-code <nh226lx@gmail.com>
2026-03-22 21:33:19 -0700

    force use real login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Auth from "./pages/Auth";
-
-function Home() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>健康系统首页</h1>
-      <div style={{ marginTop: "20px" }}>
-        <Link to="/login" style={{ marginRight: "16px" }}>
-          去登录
-        </Link>
-        <Link to="/register">去注册</Link>
-      </div>
-    </div>
-  );
-}
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Auth />} />
-      <Route path="/register" element={<Auth />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
