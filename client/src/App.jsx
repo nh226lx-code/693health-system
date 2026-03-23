994f08028498cb9968eeb1d8f02ac4c651181c50
nh226lx-code <nh226lx@gmail.com>
2026-03-22 20:18:49 -0700

    fix routes final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import DataEntry from "./pages/DataEntry";
-import Analysis from "./pages/Analysis";
+import Dashboard from "./pages/Dashboard";
+import HealthForm from "./pages/HealthForm";
+import Chart from "./pages/Chart";
+import Advice from "./pages/Advice";
+import History from "./pages/History";
+import Stats from "./pages/Stats";
 
 export default function App() {
   return (
@@ -12,8 +17,13 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/data-entry" element={<DataEntry />} />
-      <Route path="/analysis" element={<Analysis />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/data-entry" element={<HealthForm />} />
+      <Route path="/analysis" element={<Chart />} />
+      <Route path="/advice" element={<Advice />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/stats" element={<Stats />} />
     </Routes>
   );
 }
