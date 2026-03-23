61275844c7b1aa537b2e6399d467da24c5336359
nh226lx-code <nh226lx@gmail.com>
2026-03-22 20:15:38 -0700

    fix app routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DataEntry from "./pages/DataEntry";
+import Analysis from "./pages/Analysis";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/data-entry" element={<DataEntry />} />
+      <Route path="/analysis" element={<Analysis />} />
+    </Routes>
   );
 }
-
-export default App;
