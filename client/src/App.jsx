ecb98948bdd16baa7ad19db846067b71b58aa91e
nh226lx-code <nh226lx@gmail.com>
2026-03-24 03:48:19 -0700

    fix admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import AdminDashboard from "./pages/AdminDashboard"; // 👈 加这一行
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminUsers from "./pages/AdminUsers";
+import AdminRecords from "./pages/AdminRecords";
 
 export default function App() {
   return (
@@ -13,8 +15,10 @@ export default function App() {
         <Route path="/register" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
 
-        {/* 👇 关键！管理员页面 */}
+        {/* 管理员 */}
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/records" element={<AdminRecords />} />
       </Routes>
     </BrowserRouter>
   );
