ac8495bfa565d7d1ab7c8541ca253c6684e7107c
nh226lx-code <nh226lx@gmail.com>
2026-03-24 08:47:32 -0700

    fix register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminUsers from "./pages/AdminUsers";
@@ -11,12 +12,16 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        {/* 登录 / 注册 */}
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* 用户 */}
         <Route path="/dashboard" element={<Dashboard />} />
 
         {/* 管理员 */}
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/users" element={<AdminUsers />} />
         <Route path="/admin/records" element={<AdminRecords />} />
       </Routes>
