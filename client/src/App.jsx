25c9edd99eecfe71f3659c71b84a1f3bc68ac29b
nh226lx-code <nh226lx@gmail.com>
2026-03-24 02:45:46 -0700

    add admin route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import AdminDashboard from "./pages/AdminDashboard"; // 👈 加这一行
 
 export default function App() {
   return (
@@ -11,6 +12,9 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* 👇 关键！管理员页面 */}
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
