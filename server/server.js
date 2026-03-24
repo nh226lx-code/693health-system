5d3c699287e94391f28e408c47d1f09b8de1c302
nh226lx-code <nh226lx@gmail.com>
2026-03-23 21:17:40 -0700

    final deploy

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("OK");
-});
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("/api/health", (req, res) => {
   res.json({ message: "ok" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
